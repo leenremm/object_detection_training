ff7b5762559c49d8572c5f9b4a04f58d0ab7a2b4
Leen Remmelzwaal <leenremm@gmail.com>
2021-01-10 17:33:37 +0200

    Updated script to reference bndbox tag.

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -33,15 +33,16 @@ def xml_to_csv(path):
         root = tree.getroot()
         for member in root.findall("object"):
             classes_names.append(member[0].text)
+            bndbox_key = [i.tag for i in member].index("bndbox")
             value = (
                 root.find("filename").text,
                 int(root.find("size")[0].text),
                 int(root.find("size")[1].text),
                 member[0].text,
-                int(member[4][0].text),
-                int(member[4][1].text),
-                int(member[4][2].text),
-                int(member[4][3].text),
+                int(member[bndbox_key][0].text),
+                int(member[bndbox_key][1].text),
+                int(member[bndbox_key][2].text),
+                int(member[bndbox_key][3].text),
             )
             xml_list.append(value)
     column_name = [
